e9329c5a2971189cf7048d3b10180dbf886aecc0
kingbrucelee <90533878+kingbrucelee@users.noreply.github.com>
2024-04-05 23:06:16 +0200

    Zbiorczy umożliwiający korzystanie 		zmieniono:       forms.py - Praktycznie całkowity rewrite, dostosowanie do poszczególnych stron 	zmieniono:       routes.py - Nowe ścieżki, dostosowanie, zmiany wewnętrznych nazw 	zmieniono:       templates/account.html  - Dostowanie do change_credentials.html oraz delete_account.html 	zmieniono:       templates/base.html - Zamiana kolejności logowania i tworzenia konta 	zmieniono:       templates/change.html - Zmiany poszczególnych pól są teraz opcjonalne nie trzeba wszystkiego naraz 	nowy plik:       templates/change_credentials.html - Wersja dla użytkownika z potwierdzeniem poprzez wpisanie hasła 	nowy plik:       templates/delete_account.html - Wersja dla użytkownika z potwierdzeniem poprzez wpisanie hasła

--- forms.py
+++ forms.py
@@ -1,18 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Email, InputRequired, EqualTo, Length
+from wtforms.validators import Email, InputRequired, EqualTo, Length, Optional
 
-class AddAccountForm(FlaskForm):
-    login = StringField("Login", validators=[Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków"),InputRequired("Login nie może być pusty")])
-    password = PasswordField("Password", validators=[InputRequired("Hasło nie może być puste"), Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków"), EqualTo('confirm_password', message='Passwords must match')])
+class AddAccountForm(FlaskForm): # User
+    login = StringField("Login", validators=[Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków")])
+    password = PasswordField("Password", validators=[Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków"), EqualTo('confirm_password', message='Pola hasło i powtórz hasło nie są identyczne')])
     confirm_password = PasswordField("Powtórz hasło")
-    email = StringField("Email", validators=[Length(max=128, message="Email nie może mieć więcej niż 128 znaków"),Email("Proszę wprowadzić poprawny adres email")])
+    email = StringField("Email", validators=[Email("Proszę wprowadzić poprawny adres email"),Length(max=128, message="Email nie może mieć więcej niż 128 znaków")])
     submit = SubmitField("Wyślij")
 
-class DeleteAccountForm(FlaskForm):
+class RemoveAccountForm(FlaskForm): # Admin
     submit = SubmitField('Delete')
 
-class LoginForm(FlaskForm):
-    login = StringField("Login", validators=[Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków"),InputRequired("Login nie może być pusty")])
-    password = PasswordField("Password", validators=[InputRequired("Hasło nie może być puste"), Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
+class ChangeAccountForm(FlaskForm): # Admin
+    login = StringField("Login", validators=[Optional(),Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków")])
+    password = PasswordField("Password", validators=[Optional(),Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
+    email = StringField("Email", validators=[Optional(),Length(max=128, message="Email nie może mieć więcej niż 128 znaków"),Email("Proszę wprowadzić poprawny adres email")])
+    submit = SubmitField("Wyślij")
+
+class ChangeAccountCredentialsForm(FlaskForm): # User
+    login = StringField("Login", validators=[Optional(),Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków")])
+    password = PasswordField("Password", validators=[Optional(),Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
+    email = StringField("Email", validators=[Optional(),Length(max=128, message="Email nie może mieć więcej niż 128 znaków"),Email("Proszę wprowadzić poprawny adres email")])
+    old_password = PasswordField("Password", validators=[InputRequired("Stare hasło musi zostać wpisane by potwierdzić akcję"),Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
+    submit = SubmitField("Wyślij")
+
+class LoginForm(FlaskForm): # User
+    login = StringField("Login", validators=[Length(min=3, max=64, message="Login powinien mieć pomiędzy 3 a 64 znaków")])
+    password = PasswordField("Password", validators=[Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
     submit = SubmitField("Zaloguj się")
+
+class VerifyActionForm(FlaskForm): # User
+    password = PasswordField("Password", validators=[Length(min=8, max=64, message="Hasło powinno mieć pomiędzy 8 a 64 znaków")])
+    submit = SubmitField("Potwierdź")
--- routes.py
+++ routes.py
@@ -16,7 +16,7 @@ def account():
 
 @app.route('/admin')
 def admin():
-    users = User.query.all()
+    users = User.query.all() 
     return render_template("admin.html", users=users)
 
 
@@ -34,45 +34,72 @@ def login():
         flash("Błędny login lub hasło", "error")
     return render_template("login.html", form=form)
 
-@app.route('/create', methods=["GET","POST"])
+@app.route('/create', methods=["GET","POST"]) # Bulk account creation is cringe so there's no admin route of creation
 def create():
     form = forms.AddAccountForm()
     if form.validate_on_submit():
         salt = secrets.token_urlsafe(64)
-        u = User(login=form.login.data, password=hash(form.password.data,salt), salt=salt, email=form.email.data)
-        db.session.add(u)
+        user = User(login=form.login.data, password=hash(form.password.data,salt), salt=salt, email=form.email.data)
+        db.session.add(user)
         db.session.commit()
         flash("Konto zostało utworzone")
-        print("Username", form.login.data)
-        print("Password", form.password.data)
-        print("Email", form.email.data)
-        return redirect(url_for("admin"))
+        session["user_id"] = user.id
+        return redirect(url_for("account"))
     return render_template("create.html", form=form)
 
-@app.route("/change/<int:user_id>", methods=["GET","POST"])
+@app.route("/change/<int:user_id>", methods=["GET","POST"]) # Admin Change
 def change(user_id):
     user = User.query.get(user_id)
-    form = forms.AddAccountForm()
+    form = forms.ChangeAccountForm()
     if user:
         if form.validate_on_submit():
-            salt = secrets.token_urlsafe(64)
-            user.login = form.login.data
-            user.password = hash(form.password.data,salt)
-            user.salt = salt
-            user.email = form.email.data
+            if form.login.data:
+                user.login = form.login.data
+            if form.password.data:
+                salt = secrets.token_urlsafe(64)
+                user.password = hash(form.password.data,salt)    
+                user.salt = salt
+            if form.email.data:
+                user.email = form.email.data
             db.session.commit()
             flash("Poświadczenia zostały zmienione")
             return redirect(url_for("admin"))
-
-        form.login.data = user.login
-        form.email.data = user.email
         return render_template("change.html",form=form, user_id=user_id)
     flash(f"Użytkownik o numerze {user_id} nie istnieje")
     return redirect(url_for("admin"))
 
-@app.route('/delete/<int:user_id>', methods=['GET', 'POST'])
+@app.route("/change_credentials", methods=["GET","POST"]) # User Change
+def change_credentials():
+    if "user_id" in session:
+        user = User.query.get(session["user_id"])
+        form = forms.ChangeAccountCredentialsForm()
+        if user:
+            if form.validate_on_submit():
+                password_entered = hash(form.old_password.data,user.salt)
+                if password_entered == user.password:
+                        if form.login.data:
+                            user.login = form.login.data
+                        if form.password.data:
+                            salt = secrets.token_urlsafe(64)
+                            user.password = hash(form.password.data,salt)    
+                            user.salt = salt
+                        if form.email.data:
+                            user.email = form.email.data
+                        db.session.commit()
+                        flash("Poświadczenia zostały zmienione")
+                        return redirect(url_for("account"))
+                flash("Błędne stare hasło")
+            else:
+                return render_template("change_credentials.html", form=form)
+        flash("Coś poszło nie tak. Sugerowana akcja to zamknięcie i otwarcie ponownie przeglądarki")
+    else:
+        flash("Nie jesteś zalogowany")
+    return redirect(url_for("account"))
+
+
+@app.route('/delete/<int:user_id>', methods=['GET', 'POST']) # Admin Delete
 def delete(user_id):
-    form = forms.DeleteAccountForm()
+    form = forms.RemoveAccountForm()
     user = User.query.get(user_id)
     if user:
         if form.validate_on_submit():
@@ -84,3 +111,24 @@ def delete(user_id):
         return render_template('delete.html', form=form, user_id=user_id, login=user.login)
     flash(f"Użytkownik o numerze {user_id} nie istnieje")
     return redirect(url_for('admin'))
+
+@app.route('/delete_account', methods=['GET',"POST"]) # User Delete
+def delete_account():
+    form = forms.VerifyActionForm()
+    if "user_id" in session:
+        user = User.query.get(session["user_id"])
+        if user:
+            if form.validate_on_submit():
+                password_entered = hash(form.password.data,user.salt)
+                if password_entered == user.password:
+                    db.session.delete(user)
+                    db.session.commit()
+                    flash('Konto zostało usunięte')
+                    return redirect(url_for("account"))
+                flash("Błędne hasło")
+            else:
+                return render_template("delete_account.html", form=form)
+        flash("Coś poszło nie tak. Sugerowana akcja to zamknięcie i otwarcie ponownie przeglądarki")
+    else:
+        flash("Nie jesteś zalogowany")
+    return redirect(url_for('account')) 
